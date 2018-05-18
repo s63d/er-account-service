2586330a3fe38fddaf3f956d5745b6cc892fe1ed
Marvin <marvin.zwolsman@gmail.com>
2018-05-18 14:35:58 +0200

    Domain definition

---
+++ src/main/kotlin/com/s63d/eraccountservice/domain/User.kt
@@ -0,0 +1,17 @@
+package com.s63d.eraccountservice.domain
+
+import lombok.NoArgsConstructor
+import javax.persistence.*
+
+@Entity
+@NoArgsConstructor
+data class User(val email: String, val firstName: String, val lastName: String, val password: String, val address: String, val postal: String, val city: String, val role: Role, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
+
+@Entity
+@NoArgsConstructor
+data class Role(@Id val name: String, val description: String, @OneToMany val permissions: List<Permission> = emptyList())
+
+
+@Entity
+@NoArgsConstructor
+data class Permission(@Id val name: String, val description: String)
