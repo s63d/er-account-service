572c1ce9c18faf22d672467cd6a3a8f0979205ba
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-11 11:47:53 +0200

    Massively decreased key size of role and user entities

--- src/main/kotlin/com/s63d/eraccountservice/domain/User.kt
+++ src/main/kotlin/com/s63d/eraccountservice/domain/User.kt
@@ -3,7 +3,7 @@ package com.s63d.eraccountservice.domain
 import javax.persistence.*
 
 @Entity
-data class User(val email: String, var firstName: String, var lastName: String, var password: String, var address: String, var postal: String, var city: String, @OneToOne var role: Role, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
+data class User(@Column(length = 100) val email: String, var firstName: String, var lastName: String, var password: String, var address: String, var postal: String, var city: String, @OneToOne var role: Role, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
 
 @Entity
-data class Role(@Id val name: String, val description: String)
+data class Role(@Id @Column(length = 20) val name: String, val description: String)
