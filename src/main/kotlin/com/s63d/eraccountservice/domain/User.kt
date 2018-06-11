2c4acd00d59ca4312813a55fd5f9fd11a8777168
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-11 11:09:46 +0200

    fixed merge conflict

--- src/main/kotlin/com/s63d/eraccountservice/domain/User.kt
+++ src/main/kotlin/com/s63d/eraccountservice/domain/User.kt
@@ -3,7 +3,7 @@ package com.s63d.eraccountservice.domain
 import javax.persistence.*
 
 @Entity
-data class User(@Column(unique = true, length = 150) val email: String, var firstName: String, var lastName: String, var password: String, var address: String, var postal: String, var city: String, @OneToOne val role: Role, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
+data class User(@Column(unique = true, length = 150) val email: String, var firstName: String, var lastName: String, var password: String, var address: String, var postal: String, var city: String, @OneToOne var role: Role, @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0)
 
 @Entity
 data class Role(@Id val name: String, val description: String)
